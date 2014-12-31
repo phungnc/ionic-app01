9c5c873a449dc3a6bfa9870409b48531a72f2201
Phung Nguyen <phung.nc@aris-vn.com>
2014-12-31 17:41:03 +0700

    update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,28 +9,48 @@ angular.module('app.controllers', [])
   .controller('EmployeeRankController', function($scope, $stateParams, Employees) {
     $scope.employeeRank = Employees.get({employeeId: $stateParams.employeeId, data: 'rank'});
   })
-  .controller('RanksController', function($scope, $stateParams, Ranks) {
-    $scope.ranks = Ranks.query();
-    $scope.rankId = $stateParams.rankId;
-  })
+  .controller('RanksController',['$scope', '$stateParams', 'Ranks' ,'$sce',  function($scope, $stateParams, Ranks, $sce) {
 
-  .controller('RankModalController', function($scope, $ionicModal) {
+    $scope.ranks = Ranks.query(function(ranks){
+      $scope.level0 = $sce.trustAsHtml(ranks[$stateParams.rankId].level.level0);
+      $scope.level1 = $sce.trustAsHtml(ranks[$stateParams.rankId].level.level1);
+      $scope.level2 = $sce.trustAsHtml(ranks[$stateParams.rankId].level.level2);
+      $scope.level3 = $sce.trustAsHtml(ranks[$stateParams.rankId].level.level3);
+      $scope.level4 = $sce.trustAsHtml(ranks[$stateParams.rankId].level.level4);
+      $scope.level5 = $sce.trustAsHtml(ranks[$stateParams.rankId].level.level5);
+    });
+
+    $scope.rankId = $stateParams.rankId;
+  }])
+
+  .controller('RankModalController',['$scope', '$ionicModal','$sce', function($scope, $ionicModal, $sce) {
 
     //$scope.rank = Ranks.get({rankId: $stateParams.rankId});
 
-    $scope.openModal = function(level) {
-      $scope.level = level;
+    $ionicModal.fromTemplateUrl('templates/rank-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.openModal = function(level, levelId) {
+
+      //$scope.level = level;
+      $scope.levelId = levelId;
+      $scope.thisCanBeusedInsideNgBindHtml = $sce.trustAsHtml(level);
       $scope.modal.show();
+
     };
 
     $scope.closeModal = function() {
       $scope.modal.hide();
     };
 
-    $ionicModal.fromTemplateUrl('templates/rank-modal.html', function(modal) {
-      $scope.modal = modal;
-    }, {
-      animation: 'slide-in-up'
-    });
+    //$ionicModal.fromTemplateUrl('templates/rank-modal.html', function(modal) {
+    //  $scope.modal = modal;
+    //}, {
+    //  animation: 'slide-in-up'
+    //});
 
-  });
+  }]);
