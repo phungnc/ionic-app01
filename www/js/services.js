77abb4561573f0bb2ba19ac6cde2c39ca6964754
Phung Nguyen <phung.nc@aris-vn.com>
2015-01-08 17:37:16 +0700

    change server path

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('app.services', ['ngResource'])
   .factory('Employees', function ($resource) {
-    return $resource('http://demo1963560.mockable.io/employees/:employeeId/:data')
+    return $resource('http://localhost:3000/employees/:employeeId/:data')
   })
   .factory('Ranks', function ($resource) {
     return $resource('http://demo1963560.mockable.io/ranks/:rankId')
